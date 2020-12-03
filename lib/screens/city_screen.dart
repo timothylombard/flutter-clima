9ff7a9d75d67ae74f565ec3eabf89c6f61540feb
Timothy Lombard <timothylombard@gmail.com>
2020-12-03 14:38:40 +1100

    updated constants and city_screen

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -10,42 +10,48 @@ class _CityScreenState extends State<CityScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/city_background.jpg'),
-            fit: BoxFit.cover,
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/city_background.jpg'),
+                fit: BoxFit.cover,
+              )
           ),
-        ),
-        constraints: BoxConstraints.expand(),
-        child: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Icon(
-                    Icons.arrow_back_ios,
-                    size: 50.0,
+          constraints: BoxConstraints.expand(),
+          child: SafeArea(
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: FlatButton(
+                        onPressed: () {},
+                        child: Icon(
+                          Icons.arrow_back_ios,
+                          size: 50,
+                        )
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: null,
-              ),
-              FlatButton(
-                onPressed: () {},
-                child: Text(
-                  'Get Weather',
-                  style: kButtonTextStyle,
-                ),
-              ),
-            ],
+                  Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: TextField(
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                        decoration: kTextFieldInputDecoration
+                    ),
+
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Get Weather',
+                      style: kButtonTextStyle,
+                    ),
+                  ),
+                ],
+              )
           ),
-        ),
-      ),
+        )
     );
   }
 }
