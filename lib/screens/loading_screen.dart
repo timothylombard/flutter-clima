d59bed513c678c38a99b197113ba9c126f9124c7
Timothy Lombard <timothylombard@gmail.com>
2020-12-03 13:12:09 +1100

    fixed bug in loading_screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_clima/services/location.dart';
-import 'package:flutter_clima/services/networking.dart';
 import 'location_screen.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-const apiKey = 'd7485d61229ce54a64284ef4a91f22af';
-const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
-
-
+import 'package:flutter_clima/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -15,10 +9,7 @@ class LoadingScreen extends StatefulWidget {
   return _LoadingScreenState();}
 }
 
-
 class _LoadingScreenState extends State<LoadingScreen> {
-
-
   @override
   void initState() {
     super.initState();
@@ -26,18 +17,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
 
-    await location.getCurrentLocation();
-
-
-    NetworkHelper networkHelper = NetworkHelper('$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-
-    var weatherData = await networkHelper.getData();
-
-    double temperature = weatherData['main']['temp'];
-
-    print('Temp: $temperature');
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context){
       return LocationScreen(locationWeather: weatherData,);
